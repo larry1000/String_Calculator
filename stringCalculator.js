a7615a08b9477e55de05f560959846c52e5caf42
Larry L <larrylight2003@msn.com>
2020-03-14 14:51:35 -0400

    first

---
+++ stringCalculator.js
@@ -0,0 +1,5 @@
+function add(numString) {
+  if (numString === "") {
+    return 0;
+  }
+}
---
+++ stringCalculator.spec.js
@@ -0,0 +1,5 @@
+describe("stringCalculator.js", function() {
+  it("should return 0 with empty string", function() {
+    expect(add("")).toBe(0);
+  });
+});
