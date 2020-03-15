1fe0c6f0ecf60cf7566c72ecd0a7b4edab82b0e9
Larry L <larrylight2003@msn.com>
2020-03-14 20:41:03 -0400

    second

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,5 +1,52 @@
-function add(numString) {
-  if (numString === "") {
-    return 0;
+let calculator = (function() {
+  function add(numString) {
+    if (numString === "") {
+      return 0;
+    }
+
+    const delimiter = getDelimiter(numString);
+    const formattedInput = formatInput(numString);
+    return sum(getNumbers(formattedInput, delimiter));
   }
-}
+  function formatInput(input) {
+    const delimiterRegExp = /^(\/\/.*\n)/;
+    const matches = delimiterRegExp.exec(input);
+    if (matches && matches.length > 0) {
+      return input.replace(delimiterRegExp, "");
+    }
+    return input;
+  }
+
+  function getDelimiter(input) {
+    const delimiters = [];
+    const multipleDelimiterRegexp = /(?:^\/\/)?\[([^\[\]]+)\]\n?/g;
+    let matches = multipleDelimiterRegexp.exec(input);
+    while (matches !== null) {
+      delimiters.push(matches[1]);
+      matches = multipleDelimiterRegexp.exec(input);
+    }
+    if (delimiters.length > 0) {
+      return new RegExp("[" + delimiters.join("") + "]");
+    }
+    matches = /^\/\/(.*)\n/.exec(input);
+    if (matches && matches[1]) {
+      return matches[1];
+    }
+    return /[\n,]/;
+  }
+
+  function getNumbers(string, delimiter) {
+    return string
+      .split(delimiter)
+      .filter(n => n !== "")
+      .map(n => parseInt(n));
+  }
+  function sum(numbers) {
+    const finalSum = numbers.reduce((sum, n) => {
+      return sum + n;
+    }, 0);
+
+    return finalSum;
+  }
+  return { add };
+})();
--- stringCalculator.spec.js
+++ stringCalculator.spec.js
@@ -1,5 +1,13 @@
 describe("stringCalculator.js", function() {
   it("should return 0 with empty string", function() {
-    expect(add("")).toBe(0);
+    expect(calculator.add("")).toBe(0);
+  });
+
+  it('should return 1 if input is "1"', function() {
+    expect(calculator.add("1")).toBe(1);
+  });
+
+  it('should return 9 if input is "4,5"', function() {
+    expect(calculator.add("4,5")).toBe(9);
   });
 });
