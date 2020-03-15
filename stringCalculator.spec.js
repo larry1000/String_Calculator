406f53e2afc58600c74b704ba1390b080a070ebf
Larry L <larrylight2003@msn.com>
2020-03-14 20:49:02 -0400

    more tests

--- stringCalculator.spec.js
+++ stringCalculator.spec.js
@@ -10,4 +10,12 @@ describe("stringCalculator.js", function() {
   it('should return 9 if input is "4,5"', function() {
     expect(calculator.add("4,5")).toBe(9);
   });
+
+  it('should return 2 if input is "1\\n0,1"', function() {
+    expect(calculator.add("1\n0,1")).toBe(2);
+  });
+
+  it('should return 3 if input is "//;\\n1,2"', function() {
+    expect(calculator.add("//;\n1;2")).toBe(3);
+  });
 });
